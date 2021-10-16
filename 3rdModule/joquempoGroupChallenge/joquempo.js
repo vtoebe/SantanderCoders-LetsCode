6ad215b1f2231f81135778b4fd4b1cfb1f199d60
Vitória Toebe <vitoriatoebe@gmail.com>
2021-10-16 12:44:14 -0300

    Update joquempo.js

--- 3rdModule/joquempoGroupChallenge/joquempo.js
+++ 3rdModule/joquempoGroupChallenge/joquempo.js
@@ -1,61 +1,110 @@
 class Joquempo {
-    constructor(){
-        this.player1 = [];
-        this.player2 = [];
-        this.jogadas = []
+    constructor() {
+      this.player1 = [];
+      this.player2 = [];
+      this.playerHistory = []
     }
-
-    savePlays(){
-        // save player history loss/win
+  
+    savePlays() {
+      // save player history loss/win
     }
-
-    start(player1, player2){
-        this.player1 = player1;
-        this.player2 = player2;
-        this.jogadas = {player1: {name: `${player1.name}`, jogadas: `${player1._hand}`, history: {wins: 0, losses: 0}},
-                        player2: {name: `${player2.name}`, jogadas: `${player2._hand}`, history: {wins: 0, losses: 0}}
-                        };
+  
+    start(player1, player2) {
+      this.player1 = player1;
+      this.player2 = player2;
+      this.playerHistory = {
+        player1: { name: `${player1.name}`, played: `${player1._hand}`, history: { wins: 0, losses: 0 } },
+        player2: { name: `${player2.name}`, played: `${player2._hand}`, history: { wins: 0, losses: 0 } }
+      };
     }
-    
+  
+    updatePlayerHistory(player, hand) {
+      this.playerHistory[player]['played'] = [...this.playerHistory[player]['played'], hand]
+    }
+  
+    receivePlay(hand, playerName) {
+      if (this.player1.name == playerName) {
+        this.updatePlayerHistory('player1', hand)
+      } else {
+        this.updatePlayerHistory('player2', hand)
+      }
+    }
+  
     result(){
-        if (jogadas.length == 2){
-               console.log('entrou aqui')
-        }
+        let winner = [];
+          if (this.playerHistory.player1.played && this.playerHistory.player2.played !== undefined){
+              
+              if (this.player1._hand.at(-1) == 'pedra') {
+                  if (this.player2._hand.at(-1) == 'pedra') {
+                      winner = 'empate';
+                  } else if (this.player2._hand.at(-1) == 'papel') {
+                      winner = this.player2.name;
+                  } else if (this.player2._hand.at(-1) == 'tesoura') {
+                      winner = this.player1.name;
+                  }
+  
+              }
+  
+              if (this.player1._hand.at(-1) == 'papel') {
+                  if (this.player2._hand.at(-1) == 'pedra') {
+                      winner = this.player1.name;
+                  } else if (this.player2._hand.at(-1) == 'papel') {
+                      winner = 'empate';
+                  } else if (this.player2._hand.at(-1) == 'tesoura') {
+                      winner = this.player2.name;
+                  }
+              }
+  
+              if (this.player1._hand.at(-1) == 'tesoura') {
+                  if (this.player2._hand.at(-1) == 'pedra') {
+                      winner = this.player2.name;
+                  } else if (this.player2._hand.at(-1) == 'papel') {
+                      winner = this.player1.name;
+                  } else if (this.player2._hand.at(-1) == 'tesoura') {
+                      winner = 'empate';
+                  }
+              } 
+  
+              return winner == 'empate' ? 'Empatou!' : `Resultado: ${winner} venceu!`;
+          }
+      }
+  }
+  
+  class Player {
+    constructor(name) {
+      this.name = name
+      this._hand = [];
+      this.playerHistory = { wins: 0, losses: 0 };
     }
-}
-
-
-class Player {
-    constructor(name){
-        this.name = name
-        this._hand = [];
-        this.playerHistory = {wins: 0, losses: 0};
+  
+  
+    play(game, hand) {
+      this._hand.push(hand);
+      game.receivePlay(hand, this.name)
     }
-
-    play(game, hand){
-        this._hand.push(hand);
-        if (game.player1.name == this.name){
-            game.jogadas['player1']['jogadas'] = [...game.jogadas['player1']['jogadas'], hand]
-            console.log('entrou aqui')
-        } else {
-            game.jogadas['player2']['jogadas'] = [...game.jogadas['player2']['jogadas'], hand]
-            console.log('entrou aqui')
-        }
+  
+    playedHistory() {
+      // show the hand array
     }
-
-    playedHistory(){
-        // show the hand array
-    }
-}
-
-
-let vitoria = new Player('Vitoria')
-let pedro = new Player('Pedro')
-let tiago = new Player('Tiago')
-let michele = new Player('Michele')
-let jogo1 = new Joquempo()
-let jogo2 = new Joquempo()
-let jogo3 = new Joquempo()
-jogo1.start(vitoria, pedro)
-jogo2.start(vitoria, michele)
-jogo3.start(tiago, pedro)
+  }
+  
+  
+  let vitoria = new Player('Vitoria')
+  let pedro = new Player('Pedro')
+  let jogo1 = new Joquempo()
+  jogo1.start(vitoria, pedro)
+  
+  // copy/paste one by one to test conditionals
+  vitoria.play(jogo1, 'pedra')
+  pedro.play(jogo1, 'pedra')
+  jogo1.result()
+  
+  vitoria.play(jogo1, 'pedra')
+  pedro.play(jogo1, 'papel')
+  jogo1.result()
+  
+  vitoria.play(jogo1, 'tesoura')
+  pedro.play(jogo1, 'pedra')
+  jogo1.result()
+  
+  console.log(jogo1.playerHistory)
